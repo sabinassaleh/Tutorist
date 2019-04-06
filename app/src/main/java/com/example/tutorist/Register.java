bb0d4d5100edc99106bc831c90fd8715efa934ed
Saba Saleh <saba.saleh.15@cnu.edu>
2019-04-05 20:15:04 -0400

    Redirects user to BackgroundWorker when user clicks Next.

--- app/src/main/java/com/example/tutorist/Register.java
+++ app/src/main/java/com/example/tutorist/Register.java
@@ -6,10 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class Register extends AppCompatActivity {
- //   EditText firstName, lastName, email, phone;
- //   String str_firstName, str_lastName, str_email, str_phone;
+    private EditText firstName, lastName, email, password;
     private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +23,35 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                goToRegisterSubjects(v);
-            //REGISTER TO DATABASE !!!
-
             }
         });
 
-
-   //     firstName = (EditText) findViewById(R.id.editText);
-   //     lastName = (EditText) findViewById(R.id.editText2);
-   //     email = (EditText) findViewById(R.id.editText3);
-   //     phone = (EditText) findViewById(R.id.editText4);
-
+        firstName = (EditText) findViewById(R.id.tutor_firstName);
+        lastName = (EditText) findViewById(R.id.tutor_lastName);
+        email = (EditText) findViewById(R.id.t_email);
+        password = (EditText) findViewById(R.id.pass_t);
     }
  //   public void OnReg(View view) {
 
    // }
 
     public void goToRegisterSubjects(View v) {
+        String str_firstName = firstName.getText().toString();
+        String str_lastName = lastName.getText().toString();
+        String str_email = email.getText().toString();
+        String str_password = password.getText().toString();
+        String type = "signup";
+
+        //Make sure user enters all details
+        if (str_firstName.isEmpty() && str_lastName.isEmpty() && str_email.isEmpty() &&
+        str_password.isEmpty()) {
+            Toast.makeText(this, "Please provide all information", Toast.LENGTH_SHORT).show();
+        }
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, str_firstName,str_lastName,str_email,str_password);
+
+
+
         startActivity(new Intent(Register.this, RegisterSubjectsTutor.class));
-
     }
-
 }
