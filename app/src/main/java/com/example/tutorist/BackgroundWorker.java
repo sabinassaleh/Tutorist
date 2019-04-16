4a8f53c290e51ea8ad40af5ce6b0ac0564464673
Saba Saleh <saba.saleh.15@cnu.edu>
2019-04-15 23:04:52 -0400

    login page works correctly using php file.

--- app/src/main/java/com/example/tutorist/BackgroundWorker.java
+++ app/src/main/java/com/example/tutorist/BackgroundWorker.java
@@ -1,11 +1,11 @@
 package com.example.tutorist;
 
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
-
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.regions.Regions;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,34 +18,19 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 
 
-public class BackgroundWorker extends AsyncTask<String, Void, String> {
-
-    private static final String PUBLIC_DNS = "tutoristdb.c6wmb6ek3rie.us-east-2.rds.amazonaws.com";
-    private static final String PORT = "3306";
-    private static final String DATABASE = "tutoristdb";
-    private static final String REMOTE_DATABASE_USERNAME = "sssaleh";
-    private static final String DATABASE_USER_PASSWORD = "Myinstance1";
-    private static final String TABLE_NAME = "Tutor";
-
-    private static final String TABLE_STUDENT = "Student";
-
-    private static final String FIRST_NAME = "FName";
-    private static final String LAST_NAME = "LName";
-    private static final String EMAIL = "Email";
-    private static final String PASS = "Password";
-
-    private static final String TABLE_TUTOR = "Tutor";
-
+public class BackgroundWorker extends AsyncTask<String, Void, String>  {
+    AlertDialog alertDialog;
     private static final String REGISTER_URL="http://ec2-52-14-130-163.us-east-2.compute.amazonaws.com/register.php";
+    private static final String LOGIN_URL   ="http://ec2-52-14-130-163.us-east-2.compute.amazonaws.com/login.php";
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    String dataResponse = "";
+
 
     Context context;
     BackgroundWorker (Context ctx) {
@@ -54,6 +39,11 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... params) {
+        preferences = context.getSharedPreferences("MYPREFS", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        editor.putString("flag","0");
+        editor.commit();
+
         String type = params[0];
 
         if (type.equals("signup")) {
@@ -61,7 +51,10 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
             String lastName = params[2];
             String email = params[3];
             String password = params[4];
-            BufferedReader bufferedReader = null;
+            String s1 = params[5];
+            String s2 = params[6];
+            String s3 = params[7];
+
             try {
                 URL url = new URL(REGISTER_URL);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -73,15 +66,20 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
                 String myData = URLEncoder.encode("FName", "UTF-8")+"="+URLEncoder.encode(firstName, "UTF-8")
                         +"&"+URLEncoder.encode("LName", "UTF-8")+"="+URLEncoder.encode(lastName, "UTF-8")
                         +"&"+URLEncoder.encode("Email", "UTF-8")+"="+URLEncoder.encode(email, "UTF-8")
-                        +"&"+URLEncoder.encode("Password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8");
+                        +"&"+URLEncoder.encode("Password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8")
+                        +"&"+URLEncoder.encode("Subject1","UTF-8")+"="+URLEncoder.encode(s1, "UTF-8")
+                        +"&"+URLEncoder.encode("Subject2","UTF-8")+"="+URLEncoder.encode(s2, "UTF-8")
+                        +"&"+URLEncoder.encode("Subject3","UTF-8")+"="+URLEncoder.encode(s3, "UTF-8")
+                        ;
                 bufferedWriter.write(myData);
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 InputStream inputStream = conn.getInputStream();
                 inputStream.close();
 
-
-                return "Successfully Registered" + firstName + lastName;
+                editor.putString("flag","signup");
+                editor.commit();
+                return "Successfully Registered" + firstName + " " + lastName;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch(IOException e) {
@@ -91,23 +89,107 @@ public class BackgroundWorker extends AsyncTask<String, Void, String> {
         if (type.equals("login")) {
             String email = params[1];
             String pass = params[2];
+            try {
+                URL url = new URL(LOGIN_URL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+
+                //send email and pass to database
+                OutputStream outputStream = conn.getOutputStream();
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                String myData = URLEncoder.encode("Email", "UTF-8") + "="
+                        + URLEncoder.encode(email, "UTF-8") + "&"
+                        + URLEncoder.encode("Password", "UTF-8") + "="
+                        + URLEncoder.encode(pass, "UTF-8");
+                bufferedWriter.write(myData);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                //get response from database
+                InputStream inputStream = conn.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String inputLine = "";
+                while ((inputLine = bufferedReader.readLine()) != null) {
+                    dataResponse += inputLine;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                conn.disconnect();
+
+                System.out.println(dataResponse);
+
+                editor.putString("flag", "login");
+                editor.commit();
+                return dataResponse;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
 
     @Override
     protected void onPreExecute() {
-        super.onPreExecute();
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
     }
 
     @Override
-    protected void onPostExecute(String aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(String s) {
+        alertDialog.setMessage(s);
+        alertDialog.show();
 
+    /*    String flag = preferences.getString("flag","0");
+        if (flag.equals("login")) {
+            String test = "false";
+            String fName = "";
+            String lName = "";
+            String email = "";
+            String[] serverResponse = s.split("[,]");
+            test = serverResponse[0];
+            email = serverResponse[1];
+          *//*  lName = serverResponse[2];
+            email = serverResponse[3];*//*
+
+            if (test.equals("true")) {
+               *//* editor.putString("FName", fName);
+                editor.commit();
+                editor.putString("LName", lName);
+                editor.commit();*//*
+                editor.putString("Email", email);
+                editor.commit();
+                Intent intent = new Intent(context, LoginDashboard.class);
+                context.startActivity(intent);
+            }else {
+                Toast.makeText(context, "That email and/or password doesn't match our records. Please " +
+                        "check your credentials or register as a new user.", Toast.LENGTH_LONG).show();
+
+            }
+        }*/
+
+        String flag = preferences.getString("flag","0");
+        if (flag.equals("login")) {
+            if (dataResponse != null && dataResponse.equals("Login Successful. Welcome back Tutor!")) {
+                Intent intent = new Intent(context, LoginDashboard.class);
+                context.startActivity(intent);
+            } else {
+                Toast.makeText(context, "That email and/or password doesn't match our records. Please " +
+                        "check your credentials or register as a new user.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
     protected void onProgressUpdate(Void... values) {
         super.onProgressUpdate(values);
     }
+
 }
+
